0e3d324d179b764282b68f052129ec2419ec9448
Veeresh Savikeri <veeresh2004m@gmail.com>
2024-12-16 11:44:29 +0530

    added login project

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/employee");
+mongoose.connect("mongodb+srv://veeresh2004m:F6yCeIV41eVy1UBi@loginpage.jbssd.mongodb.net/?retryWrites=true&w=majority&appName=LoginPage");
 app.post('/login',(req,res)=>{
     const {email,password} = req.body
     EmployeeModel.findOne({email: email})
